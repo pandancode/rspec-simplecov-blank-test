44fbd4d621c897f5f8b476ef1d6bf5482fc795ca
yilun chen <yilun.chen@web.de>
2022-11-02 14:44:28 +0000

    Added two simple tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "User validations" do
+    it "should be valid for a user with all fields filled out" do
+      @valid_user = User.new(email: "test@test.com", password: "testtest", title: "Mr", first_name: "Jack", last_name: "Sparrow")
+      @valid_user.save
+      expect(@valid_user.valid?).to be true
+    end
+
+    it "should be invalid for a user without a title" do
+      @invalid_user = User.new(email: "test@test.com", password: "testtest", first_name: "Jack", last_name: "Sparrow")
+      @invalid_user.save
+      expect(@invalid_user.valid?).to be false
+    end
+  end
 end
